c74baacc02aa4ec7fa4d4a3cc2cc03cbd52c7720
kimjaesung <aamoos@naver.com>
2023-10-27 16:22:21 +0900

    메인 페이징 라우팅 구현 및 확인, 사용자폼, computed

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -8,21 +8,52 @@
         <label for="password">pw:</label>
         <input id="password" type="text" v-model="password" />
     </div>
-    <button type="submit">로그인</button>
+    <button :disabled="!isUsernameValid || !password" type="submit">로그인</button>
+    <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
+import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
+
 export default {
     data(){
         return{
             username : '',
-            password : ''
+            password : '',
+
+            //log
+            logMessage : '',
+        }
+    },
+    computed: {
+        isUsernameValid(){
+            return validateEmail(this.username);
         }
     },
     methods: {
-        submitForm(){
-            axios.post();
+        async submitForm(){
+            try {
+                //비즈니스 로직
+                const userData = {
+                    username: this.username,
+                    password : this.password,
+                }
+                const { data } = await loginUser(userData);
+                console.log(data.user.username);
+                this.logMessage = `${data.user.username}님 환영합니다`;
+            } catch (error) {
+                //에러 핸들링할 코드
+                // console.log(error.response.data);
+                this.logMessage = error.response.data;
+            } finally {
+                this.initForm();
+            }
+        },
+        initForm(){
+            this.username = '';
+            this.password = '';
         }
     }
 }
